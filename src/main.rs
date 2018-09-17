ce7bdb0bd88d29fff4091c1d9f74c58ce5f5587b
furuhama <furuhama@users.noreply.github.com>
2018-09-17 20:41:40 +0900

    Make udp_echo_server

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     // network::tcp_client::run();
     // network::tcp_echo_server::run();
     // network::tcp_echo_random_server::run();
-    network::lookup_host::run();
+    // network::lookup_host::run();
+    network::udp_echo_server::run();
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -2,3 +2,4 @@ pub mod tcp_client;
 pub mod tcp_echo_server;
 pub mod tcp_echo_random_server;
 pub mod lookup_host;
+pub mod udp_echo_server;

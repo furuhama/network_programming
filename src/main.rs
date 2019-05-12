15feeb44c7f8155f333fa6776bfd3decccf3c39d
furuhama <furuhama@users.noreply.github.com>
2019-05-12 21:48:18 +0900

    Move to Rust 2018 & Update crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-extern crate network_programming;
-
 #[allow(unused)]
 use network_programming::network;
 
 fn main() {
     // network::tcp_client::run();
     // network::tcp_echo_server::run();
-    // network::tcp_echo_random_server::run();
+    network::tcp_echo_random_server::run();
     // network::udp_echo_server::run();
     // network::udp_client::run();
 }
--- src/network/tcp_echo_random_server.rs
+++ src/network/tcp_echo_random_server.rs
@@ -2,7 +2,7 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::{Read, Write, Error};
 use std::time::Duration;
-use rand::{thread_rng, Rng};
+use rand::{thread_rng, seq::SliceRandom};
 
 fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
     println!("Connection has come from: {}", stream.peer_addr()?);
@@ -12,7 +12,7 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
         if bytes_read == 0 { return Ok(()) }
 
         // Set sleeping time before send response
-        let sleep = Duration::from_secs(*thread_rng().choose(&[0, 1, 2, 3, 4]).unwrap());
+        let sleep = Duration::from_secs(*[0, 1, 2, 3, 4].choose(&mut thread_rng()).unwrap());
         println!("Sleeping for {:?} seconds before send response", sleep);
 
         // Sleep for a while
